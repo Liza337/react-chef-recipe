d873544abaf01d1b7cae03f0ff22e0383373e725
Liza337 <jucse28.337@gmail.com>
2024-06-22 18:54:13 +0600

    apikey updated

--- src/Firebase/firebase.config.js
+++ src/Firebase/firebase.config.js
@@ -5,18 +5,13 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  // apiKey: import.meta.env.VITE_API_KEY,
-  // authDomain: import.meta.env.VITE_AUTH_DOMAIN,
-  // projectId: import.meta.env.VITE_PROJECT_ID,
-  // storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
-  // messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
-  // appId: import.meta.env.VITE_APP_ID
-  apiKey: "AIzaSyB4uZJ74hloEtK-oXH-ADmPo6IPOLEPCc4",
-  authDomain: "react-chef-recipe.firebaseapp.com",
-  projectId: "react-chef-recipe",
-  storageBucket: "react-chef-recipe.appspot.com",
-  messagingSenderId: "872917729420",
-  appId: "1:872917729420:web:03559b6ab52f75aafd3ba9"
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID
+ 
 
 };
 
